67d523ae6dd5cf7661f2188c52af1befddca6d67
Francisco Valdes-Jasso <koesavant@gmail.com>
2015-02-09 15:21:50 -0600

    Functionality? Yes please.

--- funk.js
+++ funk.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var howToQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
   var randomQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
   var newsQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
-
+  var current = "";
   var decide = function(elem) {
     var baseDiv = elem.parent();
     var chicken = baseDiv.parent();
@@ -14,45 +14,52 @@ $(document).ready(function(){
     switch(elem.val()){
       case '100':
       alert(cssQuestions[0]);
+      $("." + current + " option[value='100']").remove();
       return too = 2;
       break;
       case '200':
       alert(cssQuestions[1]);
+      $("." + current + " option[value='200']").remove();
       return too = 4;
       break;
       case '300':
       alert(cssQuestions[2]);
+      $("." + current + " option[value='300']").remove();
       return too = 6;
       break;
       case '400':
       alert(cssQuestions[3]);
+      $( "." + current + " option[value='400']").remove();
       return too = 8;
       break;
       case '500':
       alert(cssQuestions[4]);
+      $("." + current + " option[value='500']").remove();
       return too = 10;
       case '600':
       alert(cssQuestions[5]);
+      $("." + current + " option[value='600']").remove();
       return too = 10;
     };
   };
   $('.cssone').change(function() {
-    var numnum = $(".whisper")? 1 : 0;
-    if (numnum = 1) {
-     $(this).find('.whisper').remove();
-   };
+    current = "cssone";
    decide($(this));
  });
   $('.htmlone').change(function() {
+    current = "htmlone";
     decide($(this));
   });
   $('.newsone').change(function() {
+    current = "newsone";
     decide($(this));
   });
   $('.randomone').change(function() {
+    curent = "randomone";
     decide($(this));
   });
   $('.howtoone').change(function() {
+    current = "howtoone";
     decide($(this));
   });
 
