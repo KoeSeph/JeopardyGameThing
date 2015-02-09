50d6a5cf250201af09eea58d9151cd7502e1b8b5
Francisco Valdes-Jasso <koesavant@gmail.com>
2015-02-09 15:08:43 -0600

    Alright. Let's try this thing.
    :P

--- funk.js
+++ funk.js
@@ -1,42 +1,63 @@
 $(document).ready(function(){
-var cssQuestions = ['','','','','',''];
-var htmlQuestions = ['','','','','',''];
-var howToQuestions = ['','','','','',''];
-var randomQuestions = ['','','','','',''];
-var newsQuestions = ['','','','','',''];
+  var cssQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
+  var htmlQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
+  var howToQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
+  var randomQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
+  var newsQuestions = ['test thingerone','test thingertwo','test thingerthree','test thingerfour','test thingerfive','test thingersix'];
 
-var decide = function(elem) {
-  var baseDiv = elem.parent();
-  var chicken = baseDiv.parent();
-  chicken.removeClass('noodle');
-  baseDiv.prev().css('border-color', 'transparent');
-  console.log(elem.val());
-  switch(elem.val()){
-    case '100':
-    return too = 2;
-    break;
-    case '200':
-    return too = 4;
-    break;
-    case '300':
-    return too = 6;
-    break;
-    case '400':
-    return too = 8;
-    break;
-    case '500':
-    return too = 10;
-    case '600':
-    return too = 10;
+  var decide = function(elem) {
+    var baseDiv = elem.parent();
+    var chicken = baseDiv.parent();
+    chicken.removeClass('noodle');
+    baseDiv.prev().css('border-color', 'transparent');
+    console.log(elem.val());
+    switch(elem.val()){
+      case '100':
+      alert(cssQuestions[0]);
+      return too = 2;
+      break;
+      case '200':
+      alert(cssQuestions[1]);
+      return too = 4;
+      break;
+      case '300':
+      alert(cssQuestions[2]);
+      return too = 6;
+      break;
+      case '400':
+      alert(cssQuestions[3]);
+      return too = 8;
+      break;
+      case '500':
+      alert(cssQuestions[4]);
+      return too = 10;
+      case '600':
+      alert(cssQuestions[5]);
+      return too = 10;
+    };
   };
-};
-$('select').change(function() {
-  decide($(this));
-});
-$('.rollin').on('click', function(){
-  $('.stubborn').trigger('click');
-});
-$(".hook").on('mouseleave', function(){
-  $(this).addClass('noodle');
-});
+  $('.cssone').change(function() {
+    var numnum = $(".whisper")? 1 : 0;
+    if (numnum = 1) {
+     $(this).find('.whisper').remove();
+   };
+   decide($(this));
+ });
+  $('.htmlone').change(function() {
+    decide($(this));
+  });
+  $('.newsone').change(function() {
+    decide($(this));
+  });
+  $('.randomone').change(function() {
+    decide($(this));
+  });
+  $('.howtoone').change(function() {
+    decide($(this));
+  });
+
+
+  $(".hook").on('mouseleave', function(){
+    $(this).addClass('noodle');
+  });
 });
